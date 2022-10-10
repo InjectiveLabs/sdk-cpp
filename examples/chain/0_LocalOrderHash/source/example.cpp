4c81ab21257e213e26cc1cff57877a05df6b336c
kayo <khalid.hakimyar@outlook.com>
2022-10-10 19:04:35 +0100

    Changed style to satisfy style check

--- examples/chain/0_LocalOrderHash/source/example.cpp
+++ examples/chain/0_LocalOrderHash/source/example.cpp
@@ -1,10 +1,11 @@
 #include <injective/network.h>
+
 #include <iostream>
 
 using namespace injective;
 
 auto main() -> int {
-    const auto& network = Network::LoadNetwork("testnet","k8s");
-    std::cout << network.LcdEndpoint << std::endl;
-    return 0;
+  const auto& network = Network::LoadNetwork("testnet", "k8s");
+  std::cout << network.LcdEndpoint << std::endl;
+  return 0;
 }
--- examples/chain/1_MsgSend/source/example.cpp
+++ examples/chain/1_MsgSend/source/example.cpp
@@ -1,5 +1,3 @@
 
 
-auto main() -> int {
-    return 0;
-}
+auto main() -> int { return 0; }
