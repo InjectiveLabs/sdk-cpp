9f9427c93c265e4f31f4d27de72cdc31fb97e90a
K Omar <73123502+KAYOHB@users.noreply.github.com>
2022-10-11 09:32:33 +0100

    Changed format/style to satisfy style checks. (#3)
    * Changed format to satisfy style check.
    
    * Changed style to satisfy style check
    
    * Create cmake.yml

--- source/network.cpp
+++ source/network.cpp
@@ -3,18 +3,15 @@
 
 using namespace injective;
 
-Network Network::LoadNetwork(std::string name, std::string)
-{
-  if (name == "testnet"){
-    return Network{
-      "https://k8s.testnet.lcd.injective.network",
-      "wss://k8s.testnet.tm.injective.network/websocket",
-      "k8s.testnet.chain.grpc.injective.network:443",
-      "k8s.testnet.exchange.grpc.injective.network:443",
-      "injective-888",
-      "int",
-      "testnet"
-    }; 
+Network Network::LoadNetwork(std::string name, std::string) {
+  if (name == "testnet") {
+    return Network{"https://k8s.testnet.lcd.injective.network",
+                   "wss://k8s.testnet.tm.injective.network/websocket",
+                   "k8s.testnet.chain.grpc.injective.network:443",
+                   "k8s.testnet.exchange.grpc.injective.network:443",
+                   "injective-888",
+                   "int",
+                   "testnet"};
   }
   return Network{};
 }
