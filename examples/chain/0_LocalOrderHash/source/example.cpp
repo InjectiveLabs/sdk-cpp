1c76c2e7f0919554587480c9a208a312c85e32a4
Florin Balate <florin.balate@gmail.com>
2022-10-31 12:39:45 +0200

    * start experimenting with RESTC [WIP] * it builds using find_package, CPM does not work atm * example code crashes

--- examples/chain/0_LocalOrderHash/source/example.cpp
+++ examples/chain/0_LocalOrderHash/source/example.cpp
@@ -1,11 +1,32 @@
 #include <injective/network.h>
-
+#include "restc-cpp/restc-cpp.h"
 #include <iostream>
 
 using namespace injective;
+using namespace std;
+using namespace restc_cpp;
+
+void DoGet(Context& ctx) {
+    // Here we are in a co-routine, running in a worker-thread.
+
+    // Asynchronously connect to a server and fetch some data.
+    auto reply = ctx.Get("http://jsonplaceholder.typicode.com/posts/1");
+
+    // Asynchronously fetch the entire data-set and return it as a string.
+    auto json = reply->GetBodyAsString();
+
+    // Just dump the data.
+    cout << "Received data: " << json << endl;
+}
 
 auto main() -> int {
   const auto& network = Network::LoadNetwork("testnet", "k8s");
-  std::cout << network.LcdEndpoint << std::endl;
+  cout << network.LcdEndpoint << endl;
+
+  auto rest_client = RestClient::Create();
+
+  rest_client->Process(DoGet);
+
+  rest_client->CloseWhenReady(true);
   return 0;
 }
